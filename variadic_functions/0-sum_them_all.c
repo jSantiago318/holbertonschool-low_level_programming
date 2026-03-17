fd00bc7e754100cce7b757326e210accff5f681b
Joshua Santiago <joshuasantiago111@gmail.com>
2026-03-17 11:15:42 -0400

    making bugs variadic_functions/0-

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -5,22 +5,22 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-    unsigned int i;
-    int sum;
-    va_list args;
+unsigned int i;
+int sum;
+va_list args;
 
-    if (n == 0)
-    {
-        return (0);
-    }
+if (n == 0)
+{
+    return (0);
+}
 
-    sum = 0;
-    va_start(args, n);
-    for (i = 0; i < n; i++)
-    {
-        sum += va_arg(args, int);
-    }
-    va_end(args);
+sum = 0;
+va_start(args, n);
+for (i = 0; i < n; i++)
+{
+    sum += va_arg(args, int);
+}
+va_end(args);
 
-    return (sum);
+return (sum);
 }
