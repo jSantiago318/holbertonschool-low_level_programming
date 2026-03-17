0e0c7827fadda087af575a36b8bc7fbe7e15328c
Joshua Santiago <joshuasantiago111@gmail.com>
2026-03-17 11:07:33 -0400

    making bugs variadic_functions/0-

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -2,9 +2,6 @@
 
 /**
  * sum_them_all - returns the sum of all its parameters
- * @n: number of parameters
- *
- * Return: sum of all parameters, or 0 if n == 0
  */
 int sum_them_all(const unsigned int n, ...)
 {
