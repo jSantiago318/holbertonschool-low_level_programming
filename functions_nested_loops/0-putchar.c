0dfb99fc1a8cf71b2086a64f94c3c535146b2a82
Joshua Santiago <joshuasantiago111@gmail.com>
2026-03-19 13:58:17 -0400

    logic bugs functions/0-

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,23 +1,11 @@
-
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
 #include <stdio.h>
-
+/**
+ * main - Write a function that prints a character to stdout.
+ *
+ * Return: 0
+ */
 int main(void)
 {
-	putchar('_');
-	putchar('p');
-	putchar('u');
-	putchar('t');
-	putchar('c');
-	putchar('h');
-	putchar('a');
-	putchar('r');
-	putchar('\n');
-
-	return (0);
+printf("_putchar\n");
+return (0);
 }
-
