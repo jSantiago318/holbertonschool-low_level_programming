1404ee196330fa8261e350c4001d1032698af836
Joshua Santiago <joshuasantiago111@gmail.com>
2026-03-19 13:37:42 -0400

    logic bugs var/4-

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase, followed by a new line
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		putchar(c);
+	}
+	
+	putchar('\n');
+
+	return (0);
+}
