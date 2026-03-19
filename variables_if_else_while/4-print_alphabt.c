6c448c84d5a5d62b520aabe67036ccf7ac9f8b92
Joshua Santiago <joshuasantiago111@gmail.com>
2026-03-19 13:36:16 -0400

    logic bugs var/4-

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -11,10 +11,11 @@ int main(void)
 
 	for (c = 'a'; c <= 'z'; c++)
 	{
-        if (c != 'q' && c != 'e') {
-            putchar(c);
-        }
-	} 
+		if (c != 'q' && c != 'e')
+		{
+			putchar(c);
+		}
+	}
 	putchar('\n');
 
 	return (0);
