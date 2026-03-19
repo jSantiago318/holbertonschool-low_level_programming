23570cfd974f1116fae7bbdd07f2aa2d3c5bbb2c
Joshua Santiago <joshuasantiago111@gmail.com>
2026-03-19 15:32:53 -0400

    logic bugs functions/6--

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - computes absolute value of an integer
+ * @n: the number to check
+ *
+ * Return: the absolute value of n
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (-n);
+	}
+	else
+	{
+		return (n);
+	}
+}
