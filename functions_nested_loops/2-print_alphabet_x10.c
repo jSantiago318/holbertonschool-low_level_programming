ce81952957ffbe069d26c3fa15c0cb2fa98bd557
Joshua Santiago <joshuasantiago111@gmail.com>
2026-03-19 15:00:06 -0400

    logic bugs functions/2--

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,5 +1,17 @@
 #include "main.h"
 
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
+
+
 void print_alphabet_x10(void)
 {
 	int i;
