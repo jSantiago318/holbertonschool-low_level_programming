6ba110e27deab773671aab06f67d676394ce23a7
Joshua Santiago <joshuasantiago111@gmail.com>
2026-03-19 15:35:41 -0400

    logic bugs functions/7--

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints the last digit of a number
+ * @n: the number to check
+ *
+ * Return: the value of the last digit
+ */
+int print_last_digit(int n)
+{
+	int digit;
+
+	digit = n % 10;
+	if (digit < 0)
+		digit = -digit;
+	_putchar(digit + '0');
+	return (digit);
+}
