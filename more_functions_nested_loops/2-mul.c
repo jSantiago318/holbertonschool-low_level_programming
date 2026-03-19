446ba9d3832a57cf7d312f5241ef5af7c5205766
Joshua Santiago <joshuasantiago111@gmail.com>
2026-03-19 17:12:04 -0400

    no bugs more_functions/2-

--- more_functions_nested_loops/2-mul.c
+++ more_functions_nested_loops/2-mul.c
@@ -3,8 +3,10 @@
  * mul - multiplies two integers
  * @a: first integer
  * @b: second integer
+ *
  * Return: the product of a and b
  */
 int mul(int a, int b)
-{    return (a * b);
+{
+	return (a * b);
 }
