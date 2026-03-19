a54623bc378c6a0c12c1d2e00ece5f50b4233ac7
Joshua Santiago <joshuasantiago111@gmail.com>
2026-03-19 17:36:22 -0400

    no bugs more_functions/4-

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -4,8 +4,9 @@
 
 int _isupper(int c);
 int _isdigit(int c);
-int mul(int a, int b);  
+int mul(int a, int b);
+int _putchar(char c);
 void print_numbers(void);
-void _putchar(char c);
+void print_most_numbers(void);
 
 #endif
