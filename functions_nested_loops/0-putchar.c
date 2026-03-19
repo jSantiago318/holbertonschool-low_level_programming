1fe7c22ac5c20499b78cf5e450619d3d35aa273f
Joshua Santiago <joshuasantiago111@gmail.com>
2026-03-19 13:51:53 -0400

    logic bugs functions/0-

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: print _putchar, followed by a new line
+ */
+int main(void)
+{
+	puts("_putchar");
+	putchar('\n');
+
+	return (0);
+}
---
+++ functions_nested_loops/README.md
@@ -0,0 +1,5 @@
+# Functions, nested loops
+
+## Scripts
+
+- **0-putchar.c**: Write a function that prints a character to stdout.
