2d59447f5d18d36c208e64a80412d273d76366a9
Joshua Santiago <joshuasantiago111@gmail.com>
2026-03-19 13:23:20 -0400

    logic bugs var/2-

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase, followed by a new line
+ *
+ * Return: 0
+ */
+int main(void)
+{
+char c;
+char alphabet[] = "abcdefghijklmnopqrstuvwxyz";
+for (c = 0; c < 26; c++)
+{
+putchar(alphabet[c]);
+}
+putchar('\n');
+}
