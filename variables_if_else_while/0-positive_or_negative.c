cc1c2721b98ad426087d6d0448ac56c8ec38fc48
Joshua Santiago <joshuasantiago111@gmail.com>
2026-03-18 20:02:32 -0400

    variable_if_else_while/0-

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/*
-   * main - Entry point
-   * description: prints whether a random number is positive, negative, or zero
-   * Return: Always 0 (Success)
+/**
+
+main - Entry point
+Description: prints whether a random number is positive, negative, or zero
+Return: Always 0 (Success)
 */
+
 int main(void)
 {
 int n;
