1315ba25f3a24ffb248e710d9469a345cd799e35
Joshua Santiago <joshuasantiago111@gmail.com>
2026-03-19 13:55:33 -0400

    logic bugs functions/0-

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,14 +1,23 @@
-#include <stdio.h>
 
 /**
  * main - Entry point
  *
- * Return: print _putchar, followed by a new line
+ * Return: Always 0 (Success)
  */
+#include <stdio.h>
+
 int main(void)
 {
-	puts("_putchar");
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
 	putchar('\n');
 
 	return (0);
 }
+
