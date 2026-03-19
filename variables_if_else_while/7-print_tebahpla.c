4c3413326d97db67ca5fbcddd15cd09221c2bbd0
Joshua Santiago <joshuasantiago111@gmail.com>
2026-03-19 13:41:54 -0400

    logic bugs var/7-

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase, followed by a new line
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'z'; c >= 'a'; c--)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+
+	return (0);
+}
