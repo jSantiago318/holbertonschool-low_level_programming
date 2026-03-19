31745286adde47a37449b0a3609520fa2a691090
Joshua Santiago <joshuasantiago111@gmail.com>
2026-03-19 13:35:25 -0400

    logic bugs var/4-

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase, followed by a new line
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+        if (c != 'q' && c != 'e') {
+            putchar(c);
+        }
+	} 
+	putchar('\n');
+
+	return (0);
+}
