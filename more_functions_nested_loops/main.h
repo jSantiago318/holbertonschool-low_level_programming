6a0b8d0e78a0c35b70968a432fe152063cf65492
Joshua Santiago <joshuasantiago111@gmail.com>
2026-03-19 19:10:00 -0400

    no bugs more_functions/7-

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,7 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
@@ -10,5 +9,7 @@ void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
+void print_diagonal(int n);
 
 #endif
+
