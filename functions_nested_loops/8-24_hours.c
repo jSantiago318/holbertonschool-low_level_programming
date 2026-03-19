51f9ca00d0dbc9a974c631b9f4dfe43e46595d46
Joshua Santiago <joshuasantiago111@gmail.com>
2026-03-19 15:51:52 -0400

    logic bugs functions/8--

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute of the day of Jack Bauer
+ *
+ * Return: void
+ */
+void jack_bauer(void)
+{
+	int h, m;
+
+    /* Loop through hours and minutes, printing in HH:MM format */
+	for (h = 0; h < 24; h++)
+	{
+        /* Loop through minutes for each hour  */
+		for (m = 0; m < 60; m++)
+		{
+            /* Print the current time in HH:MM format */
+			_putchar((h / 10) + '0');
+			_putchar((h % 10) + '0');
+			_putchar(':');
+			_putchar((m / 10) + '0');
+			_putchar((m % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
