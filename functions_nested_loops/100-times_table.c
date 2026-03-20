aeededed471d258520bccdec4302d4fb3a62cf00
Joshua Santiago <joshuasantiago111@gmail.com>
2026-03-19 20:27:49 -0400

    loops/100-

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -19,26 +19,27 @@ void print_times_table(int n)
 			product = i * j;
 
 			if (j == 0)
-            {
-                _putchar('0');
-            }
+			{
+				_putchar((product % 10) + '0');
+			}
 			else
 			{
 				_putchar(',');
 				_putchar(' ');
 
-                if(
-                    product < 10
-                ){
-                    _putchar( ' ');
-                } else if (product >= 10 && product < 100)
-                {
-                    _putchar((product / 10) + '0');
-                } else if (product >= 100)
-                {
-                    _putchar((product / 100) + '0');
-                    _putchar(((product / 10) % 10) + '0');
-                }
+				if (product < 10)
+				{
+					_putchar(' ');
+				}
+				else if (product < 100)
+				{
+					_putchar((product / 10) + '0');
+				}
+				else
+				{
+					_putchar((product / 100) + '0');
+					_putchar(((product / 10) % 10) + '0');
+				}
 
 				_putchar((product % 10) + '0');
 			}
@@ -46,4 +47,5 @@ void print_times_table(int n)
 		_putchar('\n');
 	}
 }
+
 
