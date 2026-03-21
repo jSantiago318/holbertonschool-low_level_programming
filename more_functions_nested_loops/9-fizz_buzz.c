3a91b97e5733d345086f2740be0f03c7461a1569
Joshua Santiago <joshuasantiago111@gmail.com>
2026-03-21 11:05:36 -0400

    testing/proj

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -19,15 +19,8 @@ int main(void)
     int counter; 
 
     for (counter = 1; counter <= 100; counter++) {
-        if (counter % 3 == 0 && counter % 5 == 0) {
-            printf(" FizzBuzz");
-        } else if (counter % 3 == 0) {
-            printf(" Fizz");
-        } else if (counter % 5 == 0) {
-            printf(" Buzz");
-        } else {
-            printf(" %d", counter);
-        }
+        
+        write(1, " ", 1); // Print a space before each number or word
     } 
     return 0;
 }
