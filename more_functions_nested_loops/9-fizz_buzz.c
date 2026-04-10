96335abbf1b9f19c6dc168fa2201efd4a85738cf
Joshua Santiago <joshuasantiago111@gmail.com>
2026-04-10 18:41:36 -0400

    fixing fizz buzz work

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -1,26 +1,29 @@
-/*
-The "Fizz-Buzz test" is an interview question designed to help filter out the 99.5% of programming job candidates who can't seem to program their way out of a wet paper bag.
-
-Write a program that prints the numbers from 1 to 100, followed by a new line. But for multiples of three print Fizz instead of the number and for the multiples of five print Buzz. For numbers which are multiples of both three and five print FizzBuzz.
-
-    Each number or word should be separated by a space
-    You are allowed to use the standard library
-
-*/ 
 #include <stdio.h>
 
 /**
- * main - 
+ * main - prints numbers 1 to 100, Fizz for multiples of 3,
+ * Buzz for multiples of 5, and FizzBuzz for multiples of both
+ *
  * Return: Always 0.
  */
-
 int main(void)
 {
-    int counter; 
+	int i;
 
-    for (counter = 1; counter <= 100; counter++) {
-        
-        write(1, " ", 1); // Print a space before each number or word
-    } 
-    return 0;
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz");
+		else if (i % 3 == 0)
+			printf("Fizz");
+		else if (i % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%d", i);
+
+		if (i < 100)
+			printf(" ");
+	}
+	printf("\n");
+	return (0);
 }
