54294e17ba282afa67e4cf7a9dfa4bccc2e89701
Joshua SI <joshuasantiago111@gmail.com>
2026-04-24 12:02:46 -0400

    update-pointers-arrays

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _strpbrk - searches a string for any of a set of bytes
+ * @s: pointer to the string
+ * @accept: pointer to the accept string
+ *
+ * Return: pointer to the first occurrence in s of any byte in accept, or NULL
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+				return (s + i);
+		}
+	}
+
+	return (NULL);
+}
