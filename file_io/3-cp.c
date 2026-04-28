9ffb6b75af79f3aa03bf0a7d8c7d7d54a0a344d1
Joshua SI <joshuasantiago111@gmail.com>
2026-04-28 17:16:18 -0400

    adding-text-to-files-file_io

---
+++ file_io/3-cp.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+#define BUFFER_SIZE 1024
+
+/**
+ * main - copies the content of a file to another file
+ * @ac: argument count
+ * @av: argument vector
+ *
+ * Return: 0 on success
+ *         97 if incorrect number of arguments
+ *         98 if can't read from file_from
+ *         99 if can't write to file_to
+ *         100 if can't close file descriptor
+ */
+int main(int ac, char **av)
+{
+	int fd_from, fd_to;
+	char buffer[BUFFER_SIZE];
+	ssize_t n_read;
+	ssize_t n_written;
+
+	if (ac != 3)
+	{
+		dprintf(2, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	fd_from = open(av[1], O_RDONLY);
+	if (fd_from == -1)
+	{
+		dprintf(2, "Error: Can't read from file %s\n", av[1]);
+		exit(98);
+	}
+
+	fd_to = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (fd_to == -1)
+	{
+		dprintf(2, "Error: Can't write to %s\n", av[2]);
+		close(fd_from);
+		exit(99);
+	}
+
+	while ((n_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
+	{
+		n_written = write(fd_to, buffer, n_read);
+		if (n_written != n_read)
+		{
+			dprintf(2, "Error: Can't write to %s\n", av[2]);
+			close(fd_from);
+			close(fd_to);
+			exit(99);
+		}
+	}
+
+	if (n_read == -1)
+	{
+		dprintf(2, "Error: Can't read from file %s\n", av[1]);
+		close(fd_from);
+		close(fd_to);
+		exit(98);
+	}
+
+	if (close(fd_from) == -1)
+	{
+		dprintf(2, "Error: Can't close fd %d\n", fd_from);
+		exit(100);
+	}
+
+	if (close(fd_to) == -1)
+	{
+		dprintf(2, "Error: Can't close fd %d\n", fd_to);
+		exit(100);
+	}
+
+	return (0);
+}
